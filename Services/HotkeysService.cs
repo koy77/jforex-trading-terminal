f5417db996de3077db24dab92d793f250311bbc4
koy <webdevsol10@gmail.com>
2025-08-01 12:16:51 +0300

    d

--- Services/HotkeysService.cs
+++ Services/HotkeysService.cs
@@ -197,7 +197,7 @@ namespace ScreenCaptureApp.Services
                 else if (vkCode == VK_S && IsEnabled)
                 {
                     Logger.LogDebug("S key detected and service is enabled");
-                    // OnSKeyPressed?.Invoke();
+                    OnSKeyPressed?.Invoke();
                 }
                 else if (vkCode == VK_D && IsEnabled)
                 {
